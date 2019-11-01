5118d20c8af7b3cdaca35df05c5585fce17aebce
Parker Preyer <parker.preyer@gmail.com>
2019-11-01 03:35:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ function takeANumber(line, name) {
 function nowServing(line) {
   if (line.length > 0) {
     return line.shift();
-  }
+  } else {
+    return 'There is nobody waiting to be served!';
+  };
 };
